a7a8aa91809f6c1560441624b828ed563bc5b16c
5wapnill <mswapnil383@gmail.com>
2025-08-05 00:19:59 +0530

    latout done

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
-let catagories = ['Smartphone','Laptop','Automobile','Watch'];
+let catagories = ['Smartphone', 'Laptop', 'Automobile', 'Watch'];
 let catContainer = document.querySelector('.catagories');
-
-fetch('https://dummyjson.com/products/category/smartphones?limit=50')
-.then(res => res.json())
-.then(data => console.log(data));
-
-
 
 
 let searchbar = document.querySelector(".searchBar");
-console.log(searchbar);
-searchbar.placeholder = `eg. `
-
-
-
 
 catagories.forEach(item => {
     const cat = document.createElement('div');
     cat.className = 'cat';
-    cat.innerHTML = 
+    cat.innerHTML =
     `
     <img src="assets/svg/${item}.svg" width="40vw">
     <h5>${item}</h5>
     `;
     catContainer.append(cat);
+});
+
+// trying to fetch data and create element
+const Products = document.querySelector('.products');
+
+const totalProducts = ['Smartphones', 'Laptops', 'Motorcycle'];
+
+totalProducts.forEach(item => {
+
+fetch(`https://dummyjson.com/products/category/${item}?limit=3`)
+    .then(res => res.json())
+    .then(result => {
+        data = result;
+        searchbar.placeholder = `eg. ${data.products[0].title}`;
+        
+        data.products.forEach(item => {
+            const pContainer = document.createElement('div');
+            pContainer.className = 'pdiv';
+            pContainer.innerHTML =
+            `
+            <img src="${item.images[0]}"/>
+            ${item.title}
+            <br>
+            ${item.price}$
+            `
+            Products.append(pContainer);
+        })
+    });
 });
