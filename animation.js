99f2ea48e2d91ff472d7f4bc262c90d0a09af6c6
5wapnill <mswapnil383@gmail.com>
2025-08-21 12:58:01 +0530

    added home page animation

---
+++ animation.js
@@ -0,0 +1,21 @@
+let time = gsap.timeline();
+
+let dlay = 0.2;
+let dration = 0.5;
+
+
+
+time.from(".heading h1", {
+        scale: 100,
+        duration: dration + 3,
+        delay: dlay + 0.8,
+        ease: "power2.out"
+})
+
+time.from(".cat", {
+        y: 50,
+        delay: dlay,
+        duration: dration,
+        opacity: 0,
+        stagger: 0.3
+})
