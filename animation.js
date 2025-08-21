d9a2c1f7f8571ed041bae9dd9c341ac2a1ae9054
5wapnill <mswapnil383@gmail.com>
2025-08-21 17:40:03 +0530

    added blur effect

--- animation.js
+++ animation.js
@@ -8,6 +8,7 @@ let dration = 0.5;
 time.from(".heading h1", {
         y:100,
         scale: 3,
+        filter: "blur(20px)",
         opacity:0,
         duration: dration + 1,
         delay: dlay + 0.8,
@@ -19,5 +20,6 @@ time.from(".cat", {
         delay: dlay,
         duration: dration,
         opacity: 0,
-        stagger: 0.3
+        stagger: 0.3,
+        filter: "blur(20px)",
 })
