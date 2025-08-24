95eafc07568c4994b394afcf90b141504ef6063b
5wapnill <mswapnil383@gmail.com>
2025-08-25 00:12:45 +0530

    added animation on .pdiv

--- animation.js
+++ animation.js
@@ -1,25 +1,58 @@
-let time = gsap.timeline();
+gsap.registerPlugin(ScrollTrigger);
 
+let time = gsap.timeline();
 let dlay = 0.2;
 let dration = 0.5;
 
+heroPage();
 
+setTimeout(() => {
+        items();
+        ScrollTrigger.refresh();
+}, 1000);
 
-time.from(".heading h1", {
-        y:100,
-        scale: 3,
-        filter: "blur(20px)",
-        opacity:0,
-        duration: dration + 1,
-        delay: dlay + 0.8,
-        ease: "power2.out"
-})
+function heroPage() {
+        time.from(".heading h1", {
+                y: 100,
+                scale: 3,
+                filter: "blur(20px)",
+                opacity: 0,
+                duration: dration + 1,
+                delay: dlay + 0.8,
+                ease: "power2.out"
+        })
 
-time.from(".cat", {
-        y: 50,
-        delay: dlay,
-        duration: dration,
-        opacity: 0,
-        stagger: 0.3,
-        filter: "blur(20px)",
-})
+        time.from(".cat", {
+                y: 50,
+                delay: dlay,
+                duration: dration,
+                opacity: 0,
+                stagger: 0.3,
+                filter: "blur(20px)",
+        })
+}
+
+function items() {
+        ScrollTrigger.batch(".section-heading", {
+                onEnter: (elements) => {
+                        gsap.from(elements, {
+                                opacity: 0,
+                                y: 50,
+                                duration: 1,
+                                ease: "power2.out"
+                        });
+                }
+        });
+
+        ScrollTrigger.batch(".pdiv", {
+                onEnter: (elements) => {
+                        gsap.from(elements, {
+                                opacity: 0,
+                                y: 50,
+                                duration: 1,
+                                ease: "power2.out",
+                                rotate: 10,
+                        });
+                }
+        });
+}
